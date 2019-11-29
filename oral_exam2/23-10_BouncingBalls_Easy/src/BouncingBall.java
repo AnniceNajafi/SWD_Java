84b6977a6b705bf9d7e01c20a4d78148d247bc16
Annice Najafi <annice-najafi@uiowa.edu>
2019-11-28 20:21:43 -0600

    Bouncing Ball done...

--- oral_exam2/23-10_BouncingBalls_Easy/src/BouncingBall.java
+++ oral_exam2/23-10_BouncingBalls_Easy/src/BouncingBall.java
@@ -6,13 +6,47 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class BouncingBall extends ImageIcon{
-
+public class BouncingBall extends JPanel implements Runnable{
+    int x, y, dx, dy;
+    private static final int panel_WIDTH = 640;
+    private static final int panel_HEIGHT = 480;
     public BouncingBall(){
-        ImageIcon blueBall = new ImageIcon("Blue_Ball.jpg");
+        x=0;
+        y=0;
+        dx=1;
+        dy=1;
     }
-    public void move(){
-
+    @Override
+    public void paint(Graphics g) {
+        super.paintComponents(g);
+        this.setPreferredSize(new Dimension(panel_WIDTH, panel_HEIGHT));
+        g.setColor(Color.BLUE);
+        g.fillOval(x,y,20,20);
     }
-
+    public void move() {
+        if(x+dx<0){
+            dx=1;
+        }else if(x+dx>getWidth() - 20){
+            dx=-1;
+        }else if(y+dy<0){
+            dy=1;
+        }else if(y+dy>getHeight() - 20){
+            dy=-1;
+        }
+          x=x+dx;
+          y=y+dy;
+          repaint();
+    }
+    @Override
+    public void run(){
+        while(true){
+            move();
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- oral_exam2/23-10_BouncingBalls_Easy/src/BouncingBallTest.java
+++ oral_exam2/23-10_BouncingBalls_Easy/src/BouncingBallTest.java
@@ -1,8 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class BouncingBallTest {
-    public static void main(String[] args) throws IOException {
-        BouncingBallPlace test;
-        test = new BouncingBallPlace();
+    public static void main(String[] args) {
+        JFrame place = new JFrame("Bouncing Ball");
+        place.setSize(500,500);
+        place.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BouncingBall ball = new BouncingBall();
+        place.add(ball);
+        place.setVisible(true);
+        place.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                ExecutorService executorService = Executors.newCachedThreadPool();
+                executorService.execute(ball);
+            }
+        });
     }
 }
